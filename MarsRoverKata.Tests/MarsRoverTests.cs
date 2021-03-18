29d45e6037ee435e18e4a61d986be3214063adc6
Siphiwe Manda <siphiwe.manda@bepsystems.co.uk>
2021-03-18 17:09:18 +0000

    Add project files.

---
+++ MarsRoverKata.Tests/MarsRoverTests.cs
@@ -0,0 +1,68 @@
+﻿using Xunit;
+
+namespace MarsRoverKata.Tests
+{
+    public class MarsRoverTests
+    {
+        [Fact]
+        public void ARoverIsAtZeroZeroSouthWhenInitialized()
+        {
+            //Arrange
+            var rover = new Rover();
+            //Act
+            var position = rover.Execute("");
+            //Assert
+            Assert.Equal("0:0:S", position);
+        }
+        //[Fact]
+        // public void ARoverMovesOnePositionWhenRecivingTheCommandM()
+        // {
+        //     //Arrange
+        //     var rover = new Rover();
+        //     //Act
+        //     var position = rover.Execute("M");
+        //     //Assert
+        //     Assert.Equal("0:1:S", position);
+        // }
+        [Fact]
+        public void ARoverPointsWestAfterTheCommandR()
+        {
+            //Arrange
+            var rover = new Rover();
+            //Act
+            var position = rover.Execute("R");
+            //Assert
+            Assert.Equal("0:0:W", position);
+        }
+        [Fact]
+        public void ARoverPointsEastAfterTheCommandL()
+        {
+            //Arrange
+            var rover = new Rover();
+            //Act
+            var position = rover.Execute("L");
+            //Assert
+            Assert.Equal("0:0:E", position);
+        }
+        [Fact]
+        public void ARoverPointsNorthAfterTheCommandRR()
+        {
+            //Arrange
+            var rover = new Rover();
+            //Act
+            var position = rover.Execute("RR");
+            //Assert
+            Assert.Equal("0:0:N", position);
+        }
+        [Fact]
+        public void ARoverPointsEastAfterTheCommandRRR()
+        {
+            //Arrange
+            var rover = new Rover();
+            //Act
+            var position = rover.Execute("RRR");
+            //Assert
+            Assert.Equal("0:0:E", position);
+        }
+    }
+}
---
+++ MarsRoverKata/Rover.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace MarsRoverKata
+{
+    public class Rover
+    {
+        public string Direction { get; set; }
+
+        public Rover()
+        {
+            Direction = "S";
+        }
+
+        public string Execute(string commands)
+        {
+
+            foreach (var command in commands)
+            {
+                SetDirection(command);
+            }
+           
+
+            // if (commands == string.Empty)
+            // {
+            //     return $"0:0:{Direction}";
+            // }
+           return $"0:0:{Direction}";
+        }
+
+        public void SetDirection(char command)
+        {
+            if (command == 'R')
+                switch (Direction)
+                {
+                    case "S":
+                        Direction = "W";
+                        break;
+                    case "W":
+                        Direction = "N";
+                        break;
+                    case "N":
+                        Direction = "E";
+                        break;
+                    case "E":
+                        Direction = "W";
+                        break;
+                }
+
+            if (command == 'L')
+                switch (Direction)
+                {
+                    case "S":
+                        Direction = "E";
+                        break;
+                    case "W":
+                        Direction = "S";
+                        break;
+                    case "N":
+                        Direction = "W";
+                        break;
+                    case "E":
+                        Direction = "N";
+                        break;
+                }
+        }
+
+    
+    }
+}
